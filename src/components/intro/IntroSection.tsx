aee7b2b66a2c451fc301068582e5b4395ff58162
Ixyz.dev <ixyz.dev@gmail.com>
2024-04-10 23:03:53 -0400

    Actualizacion previa a vista

--- src/components/intro/IntroSection.tsx
+++ src/components/intro/IntroSection.tsx
@@ -1,34 +1,38 @@
 import React from 'react'
-// Asegúrate de que Button está importado correctamente según tu estructura de archivos
-import { Button } from '@/components/ui/button'
+// import { Button } from '@/components/ui/button'
 
 function IntroSection(): React.JSX.Element {
-  const navigationButtons = [
-    'PRELUDIO',
-    'PROYECTO',
-    'COLECCIONES',
-    'EQUIPO',
-    'NOTICIAS',
-    'CONTACTO'
-  ]
+  // const navigationButtons = [
+  //   'PRELUDIO',
+  //   'PROYECTO',
+  //   'COLECCIONES',
+  //   'EQUIPO',
+  //   'NOTICIAS',
+  //   'CONTACTO'
+  // ]
 
   return (
     <div className="">
-      <nav className="flex justify-center py-4 overflow-auto">
+      {/* Sección de navegación */}
+      {/* <nav className="flex justify-center py-4 overflow-auto w-full">
         <div className="flex space-x-4 max-w-full flex-nowrap whitespace-nowrap">
           {navigationButtons.map((buttonText) => (
-            <Button key={buttonText} variant="outline">
+            <Button key={buttonText} variant="outline" size={'sm'}>
               {buttonText}
             </Button>
           ))}
         </div>
-      </nav>
-      <div className="text-center py-10">
-        <h1 className="text-4xl font-bold mb-2">
+      </nav> */}
+      {/* Contenido de la sección de introducción */}
+      <div className="text-center py-10 mx-auto">
+        {/* Título de la sección */}
+        <h1 className="text-4xl font-bold mb-2 ">
           Bienvenidos a la biblioteca de modelos 3D
         </h1>
-        <hr className="border-gray-400 mx-auto w-1/4" />
-        <p className="mt-4 mx-auto leading-relaxed max-w-4xl text-lg">
+        {/* Subtítulo de la sección */}
+        <hr className="border-gray-400 mx-auto w-3/4 m" />
+
+        <p className="mt-4 leading-relaxed text-lg mx-32">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas
           enim a nulla faucibus maximus. Nunc non ligula gravida, pellentesque
           ipsum eu, rutrum justo. Aenean et ipsum eros. Mauris porta turpis
