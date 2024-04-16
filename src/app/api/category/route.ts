fadd879172be0bc9364177847f64ae58c4b1d704
Ixyz.dev <ixyz.dev@gmail.com>
2024-04-16 00:06:48 -0400

    Solucionando problemas con conectividada

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,19 +1,27 @@
 import axios from 'axios'
-import { type NextApiRequest, type NextApiResponse } from 'next'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> {
-  if (req.method === 'GET') {
-    try {
-      const response = await axios.get('https://localhost:3001/category')
-      res.status(200).json(response.data)
-    } catch (error) {
-      res.status(500).json({ error: 'Error al obtener los datos' })
-    }
-  } else {
-    res.setHeader('Allow', ['GET'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+interface Params {
+  id: string
+}
+
+export const GET = async (
+  req: Request,
+  { params }: { params: Params }
+): Promise<Response> => {
+  try {
+    const { data } = await axios.get(
+      `http://127.0.0.1:8000/eventApp/api/events/${params.id}`,
+      {
+        headers: {
+          'X-CSRFToken': process.env.CSRF_TOKEN
+        }
+      }
+    )
+
+    return new Response(JSON.stringify(data), { status: 200 })
+  } catch (error: any) {
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 500
+    })
   }
 }
