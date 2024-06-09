f00c977cdd8b2e720b15c17b776140dc4df086c0
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-09 09:35:34 -0400

    Arreglando searchBar

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { IoSearch as SearchIcon } from 'react-icons/io5'
 import { Input } from '@/components/ui/input'
 
-function SearchBar({ className }: { className?: string }): React.JSX.Element {
+const SearchBar: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div className={className}>
       <div className="relative rounded-lg dark:bg-gray-800">
         <SearchIcon className="absolute inset-y-0 inset-x-2.5 top-2.5 translate-x-0.5 h-4 w-4 text-gray-400 dark:text-gray-500" />
         <Input
-          className="bg-transparent appearance-none pl-8  bg-gray-100"
+          className="bg-transparent appearance-none pl-8 bg-gray-100"
           placeholder="Search"
           type="search"
         />
