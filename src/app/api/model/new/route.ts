3b67a86c778e64228b5637c4fb3908fded6c1e9e
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-09 10:00:30 -0400

    Arreglando ruta vacia

--- src/app/api/model/new/route.ts
+++ src/app/api/model/new/route.ts
@@ -1,39 +1,34 @@
-// /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-// import axios from 'axios'
-// import type { NextApiRequest, NextApiResponse } from 'next'
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import axios from 'axios'
+import { type NextRequest, NextResponse } from 'next/server'
 
-// export const POST = async (
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ): Promise<void> => {
-//   try {
-//     if (req.method !== 'POST') {
-//       res.setHeader('Allow', ['POST'])
-//       res.status(405).end(`Method ${req.method} Not Allowed`)
-//       return
-//     }
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json()
 
-//     console.log('Request body:', req.body)
+    console.log('Request body:', body)
 
-//     const response = await axios.post(
-//       'http://localhost:3001/publications',
-//       req.body
-//     )
+    const response = await axios.post(
+      'http://localhost:3001/publications',
+      body
+    )
 
-//     console.log('Response from backend:', response.data)
+    console.log('Response from backend:', response.data)
 
-//     res.status(200).json(response.data)
-//   } catch (error: any) {
-//     console.error(
-//       'Error:',
-//       error.response ? error.response.data : error.message
-//     )
+    return NextResponse.json(response.data, { status: 200 })
+  } catch (error: any) {
+    console.error(
+      'Error:',
+      error.response ? error.response.data : error.message
+    )
 
-//     res.status(500).json({
-//       message: 'Internal Server Error',
-//       error: error.response ? error.response.data : error.message
-//     })
-//   }
-// }
-
-// export default POST
+    return NextResponse.json(
+      {
+        message: 'Internal Server Error',
+        error: error.response ? error.response.data : error.message
+      },
+      { status: 500 }
+    )
+  }
+}
