a1dcf0429f5ea70e6ac78e4fbea55d512e1e518f
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-09 10:24:14 -0400

    A

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import React, { useState, useEffect, Suspense } from 'react'
-import { type IArticle } from '@/interfaces/IArticle'
 import { useSearchParams } from 'next/navigation'
-
+import { type IArticle } from '@/interfaces/IArticle'
 import ArticlePage from '@/components/article/ArticlePage'
 
-function ModelDetails({ id }: { id: string | null }): React.JSX.Element {
+function ModelDetails(): React.JSX.Element {
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+
   // Estado del modelo
   const [model, setModel] = useState<IArticle | null>(null)
-
   // Estados de carga y error
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string | null>(null)
@@ -57,17 +58,10 @@ function ModelDetails({ id }: { id: string | null }): React.JSX.Element {
   return <ArticlePage model={model} />
 }
 
-export default function ModelDetailsPage({
-  params
-}: {
-  params: { id: string }
-}): React.JSX.Element {
-  const searchParams = useSearchParams()
-  const id = searchParams.get('id')
-
+export default function ModelDetailsPage(): React.JSX.Element {
   return (
     <Suspense fallback={<div>Cargando...</div>}>
-      <ModelDetails id={id} />
+      <ModelDetails />
     </Suspense>
   )
 }
