e4fff674b53d8c8ef8ffddb966f06c324fd20b4a
Ixyz.dev <ixyz.dev@gmail.com>
2024-08-20 22:48:58 -0400

    Solucionando implementacion de la integracion de apiClient

--- src/lib/apiClient/apiClient.ts
+++ src/lib/apiClient/apiClient.ts
@@ -10,7 +10,7 @@ import axios from 'axios'
 */
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.LEARN_HORIZON_BACKEND_REMOTE ?? 'http://localhost:3001',
   timeout: 5000
   // Puedes agregar más configuraciones aquí según sea necesario
 })
