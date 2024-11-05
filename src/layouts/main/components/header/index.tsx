32ce30dad30a6ea4b5ecd9d27e3d2315ea98fa96
Ixyz.dev <ixyz.dev@gmail.com>
2024-11-05 13:27:37 -0300

    Mejorando layout para la navbar

--- src/layouts/main/components/header/index.tsx
+++ src/layouts/main/components/header/index.tsx
@@ -2,21 +2,26 @@ import React from 'react'
 
 // * Importar componentes
 import HeaderLogo from './HeaderLogo'
-import HeaderLinks from './HeaderLinks'
+// import HeaderLinks from './HeaderLinks'
 
 import Profile from '@/layouts/shared/profile/index'
 
-const Header: React.FC = () => {
-  /*
-   * Componente que se encarga de renderizar el header de la aplicación
-   TODO: Implementar el estado del usuario para renderizar los componentes correspondientes utilizando NextAuth
-   */
+interface HeaderProps {
+  className?: string
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
+  const { className } = props
 
   return (
-    <nav className="sticky top-0 left-0 right-0 z-50 bg-white border-b shadow flex items-center h-14 px-4">
-      <HeaderLogo />
-      <HeaderLinks />
-      <Profile />
+    <nav
+      className={`${className} sticky top-0 left-0 right-0 z-50 bg-white border-b shadow flex items- h-14 px-4`}
+    >
+      <section className="flex justify-between w-full">
+        <HeaderLogo />
+        {/* <HeaderLinks /> */}
+        <Profile />
+      </section>
     </nav>
   )
 }
